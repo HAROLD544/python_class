2cea55056ca7bbcb4c88b88ce37d9b6f67d52d32
HAROLD544 <haroldmolina98@gmail.com>
2018-08-27 18:11:09 -0500

    promedio de edades

---
+++ academico.py
@@ -0,0 +1,20 @@
+#suma y promedio de edades
+sumaedades=0
+promedio=0
+edad1=15
+persona1="Juan Carlos Arteaga"
+edad2=18
+persona2="Angela Maria Jurado"
+edad3=22
+persona3="Juan Carlos Arteaga"
+edad4=19
+persona4="Monica Maribel Hurtado"
+print ("      SISTEMA DE INFORMACION ACADEMICO - PROMEDIO EDADES")
+sumaedades=edad1+edad2+edad3+edad4
+print ("el estudiante",persona1,"tiene",edad1,"años")
+print ("el estudiante",persona2,"tiene",edad2,"años")
+print ("el estudiante",persona3,"tiene",edad3,"años")
+print ("el estudiante",persona4,"tiene",edad4,"años")
+promedio=sumaedades/4
+print ("el promedio de edades es: ",promedio)
+
