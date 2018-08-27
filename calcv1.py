59893fa5dedd6c68293e4a369c7803360822cdea
HAROLD544 <haroldmolina98@gmail.com>
2018-08-27 17:46:16 -0500

    primera practica calculadora

---
+++ calcv1.py
@@ -0,0 +1,11 @@
+#primera practica calculadora
+num1=10
+num2=5
+suma=num1+num2
+resta=num1-num2
+mult=num1*num2
+div=num1/num2
+print ("la suma es:",suma)
+print ("la resta es:",resta)
+print ("la multiplicacion es:",mult)
+print ("la division es:",div)
