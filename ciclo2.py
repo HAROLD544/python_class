d23582fc036822960e36549a1a72ca2c5a3bd465
HAROLD544 <haroldmolina98@gmail.com>
2018-09-05 12:21:05 -0500

    tablas de multiplicar de 1 a 10

---
+++ ciclo2.py
@@ -0,0 +1,12 @@
+
+rango= 1
+tabla=1
+while tabla<=10 :
+    rango =1
+    while rango<=10 :
+        print (tabla, "*", rango, "=", rango*tabla)
+        rango=  rango+1
+    tabla=tabla+1
+    
+
+
