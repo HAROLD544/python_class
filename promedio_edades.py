3c575b89566a7b201dee911b5eb70c2a50b9cd6b
HAROLD544 <haroldmolina98@gmail.com>
2018-09-05 13:42:28 -0500

    promedio de edades

---
+++ promedio_edades.py
@@ -0,0 +1,41 @@
+x=1
+sumaedades=0
+while x <= 10 :
+    edad = int(input("ingrese su edad :"))
+    sumaedades = sumaedades + edad
+    x = x + 1
+promedio=sumaedades/10
+print("su promedio es:",promedio)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
