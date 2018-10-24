913697a053b387c4b36683a9fcf977fe17dddf15
“HAROLD544” <haroldmolina98@gmail.com>
2018-10-24 14:00:51 -0500

    calculadora

---
+++ myWindow1.py
@@ -0,0 +1,11 @@
+#Importar librerias para GUI
+from Tkinter import *
+import Tkinter
+#######################
+root = Tkinter.Tk()
+root.geometry("400x400")
+root.title("mi calculadora")
+root.resizable(FALSE,FALSE)
+#######################
+root.mainloop()
+
