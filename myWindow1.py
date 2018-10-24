2f7a264b39e5f93558e36d8a53ffc4d94b437137
“HAROLD544” <haroldmolina98@gmail.com>
2018-10-24 14:43:34 -0500

    calculadora

--- myWindow1.py
+++ myWindow1.py
@@ -6,6 +6,9 @@ root = Tkinter.Tk()
 root.geometry("400x400")
 root.title("mi calculadora")
 root.resizable(FALSE,FALSE)
+root.configure(background="#17A589")
 #######################
+display=Entry(root,font=('arial',20,'bold'),width=22,bd=20,insertwidth=10,bg="#A93226",justify="right").place(x=10,y=60)
+boton1=button(root,text="1",width=7,height=3).place(x=10,y=60)
 root.mainloop()
 
